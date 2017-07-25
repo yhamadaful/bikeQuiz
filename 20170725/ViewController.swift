94abc8d8449eceb899b3f8722d4e6dd37b6cf13d
nttr <nttr@BI-23569.local>
2017-07-25 10:48:29 +0900

    Initial Commit

---
+++ 20170725/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  20170725
+//
+//  Created by nttr on 2017/07/25.
+//  Copyright © 2017年 nttr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
